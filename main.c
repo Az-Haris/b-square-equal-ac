f3f3fde423605d906a9963d2526782185d3b562d
Az-Haris <73691596+Az-Haris@users.noreply.github.com>
2021-12-24 15:34:14 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b, c;
+    printf("Enter Three Number : ");
+    scanf("%d%d%d", &a,&b,&c);
+    (b*b==a*c)?printf("Proportional"):printf("Not Proportional");
+    return 0;
+}
